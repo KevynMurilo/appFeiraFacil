3bd997dfa4872122347b6637dd3f8bf36718cc60
KevynMurilo <kevynmurilo2017@gmail.com>
2025-06-10 00:17:23 -0300

    Update

--- app.config.js
+++ app.config.js
@@ -5,17 +5,38 @@ export default ({ config }) => {
   const apiUrl =
     environment === 'prod'
       ? process.env.URL_BASE_PROD
-      : process.env.URL_BASE_DEV_JOB || 'http://10.1.59.59/api';
+      : process.env.URL_BASE_DEV_HOME || 'http://177.85.251.66/api';
 
   return {
-    ...config,
     name: 'FeiraFacil',
     slug: 'appfeirafacil',
+    owner: 'nyv3kexpo', // ← AQUI É O QUE FALTAVA
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/1x1.png',
+      resizeMode: 'cover',
+      backgroundColor: '#004AAD'
+    },
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      package: 'com.prefeitura.goias',
+      versionCode: 1,
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff'
+      },
+      edgeToEdgeEnabled: true
+    },
     extra: {
       API_URL: apiUrl,
       eas: {
-        projectId: '6fe95fd5-b01b-4ce1-acea-8447f570ec64',
-      },
-    },
+        projectId: '5070b66c-a01e-44ad-a016-8650f80270bd'
+      }
+    }
   };
 };
